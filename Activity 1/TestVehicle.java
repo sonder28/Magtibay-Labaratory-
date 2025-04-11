0b980ff25608145b5e2b32666b12d08e8853d13a
sonder28 <magtibayks@students.nu-lipa.edu.ph>
2025-04-11 12:39:18 +0800

    add

---
+++ Activity 1/TestVehicle.java
@@ -0,0 +1,11 @@
+public class TestVehicle{
+    public static void main(String [] args){
+        private String brand;
+        private Double speed;
+        private String FuelType;
+
+        public class Vehicle{
+           
+        }
+
+
--- Activity 1/Vehicle.java
+++
@@ -1,5 +0,0 @@
-public class Vehicle{
-    public static void main(String [] args){
-        System.out.println("Hello nigger");
-    }
-}
